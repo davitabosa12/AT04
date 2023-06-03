38a68e0a2a8ca9a16d212e15596b6b829d6d4c18
Davi Tabosa <davitabosa12@hotmail.com>
2023-06-03 15:51:11 -0300

    remove dog feature

--- dogapi/servidor-rest/src/main/java/br/ufc/at04/MainController.java
+++ dogapi/servidor-rest/src/main/java/br/ufc/at04/MainController.java
@@ -6,10 +6,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,8 +19,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.ufc.at04.dto.ExceptionDTO;
 import br.ufc.at04.dto.BreedImageDTO;
 import br.ufc.at04.dto.BreedSummaryDTO;
+import br.ufc.at04.exception.BreedNotFoundException;
 import br.ufc.at04.model.BreedEntity;
 import br.ufc.at04.repository.BreedRepository;
 import br.ufc.at04.service.DogAPIService;
@@ -57,6 +61,19 @@ public class MainController {
 	public @ResponseBody ResponseEntity<BreedSummaryDTO> getDog(@PathVariable("breed") String breedName) {
 		Optional<BreedEntity> breed = breedRepository.findByBreedName(breedName);
 		return ResponseEntity.ofNullable(breed.map(b -> new BreedSummaryDTO(b)).orElseGet(() -> null));
+	}
+
+	@CrossOrigin(origins = "*")
+	@DeleteMapping("{breed}")
+	public @ResponseBody ResponseEntity<Void> removeDog(@PathVariable("breed") String breedName) {
+		BreedEntity breed = breedRepository.findByBreedName(breedName).orElseThrow(BreedNotFoundException::new);
+		breedRepository.delete(breed);
+		return ResponseEntity.noContent().build();
+	}
+
+	@ExceptionHandler(BreedNotFoundException.class)
+	public ResponseEntity<ExceptionDTO> handleBreedNotFound(BreedNotFoundException ex) {
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ExceptionDTO(ex));
 	}
 
 	@CrossOrigin(origins = "*")
